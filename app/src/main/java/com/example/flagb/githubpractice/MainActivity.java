75cc41ff7b4604c0c1679cb74419ce2193092982
Park-JaeHyun <flagbug12@gmail.com>
2017-07-15 19:33:40 +0900

    init

--- app/src/main/java/com/example/flagb/githubpractice/MainActivity.java
+++ app/src/main/java/com/example/flagb/githubpractice/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.flagb.githubpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        editText = (EditText) findViewById(R.id.textview1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
